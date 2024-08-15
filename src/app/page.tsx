21f932ff908e2a03e1b5b29b628f2e9414881053
“jakhongirav” <jakhongir.av@gmail.com>
2024-08-15 23:12:23 +0500

    making page work in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
-import Image from "next/image";
+import { ChatWrapper } from "@/components/ChatWrapper";
+import { ragChat } from "@/lib/rag-chat";
+import { redis } from "@/lib/redis";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const sessionCookie = cookies().get("sessionId")?.value;
+  const sessionId = sessionCookie || "default-session";
+
+  const isAlreadyIndexed = await redis.sismember("indexed-urls", "home");
+
+  const initialMessages = await ragChat.history.getMessages({
+    amount: 10,
+    sessionId,
+  });
+
+  if (!isAlreadyIndexed) {
+    await ragChat.context.add({
+      type: "text",
+      data: "This is the home page of the website.",
+      options: { metadata: { page: "home" } },
+    });
+
+    redis.sadd("indexed-urls", "home");
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
   );
 }
